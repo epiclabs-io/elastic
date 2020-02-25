9e984ffb93d8f620d491afa2d8dea295e95fc7e0
Javier Peletier <jm@epiclabs.io>
2020-02-26 00:22:02 +0100

    moved over source

---
+++ convert.go
@@ -0,0 +1,265 @@
+package elastic
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+type ConverterFunc func(source interface{}, targetType reflect.Type) (interface{}, error)
+type ConverterTo interface {
+	ConvertTo(targetType reflect.Type) (interface{}, error)
+}
+
+type ConverterEngine struct {
+	sourceConverters    map[reflect.Type][]ConverterFunc
+	targetConverters    map[reflect.Type][]ConverterFunc
+	interfaceConverters map[reflect.Type][]ConverterFunc
+}
+
+// Default is a default conversion engine
+var Default = New()
+
+// ErrExpectedPointer is returned when the function expects a pointer parameter
+var ErrExpectedPointer = errors.New("Expected pointer")
+
+// ErrIncompatibleType is returned when it is impossible to convert a type to another
+var ErrIncompatibleType = errors.New("Incompatible types")
+
+// ErrNoConversionAvailable is returned by any ConverterFunc when it does not know how to convert the passed values
+var ErrNoConversionAvailable = errors.New("No conversion available")
+
+// New instantiates a new Converter Engine
+func New() *ConverterEngine {
+	return &ConverterEngine{
+		sourceConverters:    make(map[reflect.Type][]ConverterFunc),
+		targetConverters:    make(map[reflect.Type][]ConverterFunc),
+		interfaceConverters: make(map[reflect.Type][]ConverterFunc),
+	}
+}
+
+// AddSourceConverter adds a source conversion function to the engine that knows how to convert the source type to some targets
+func (ce *ConverterEngine) AddSourceConverter(sourceType reflect.Type, f ConverterFunc) {
+	cf := ce.sourceConverters[sourceType]
+	cf = append(cf, f)
+	ce.sourceConverters[sourceType] = cf
+}
+
+// AddTargetConverter adds a target conversion function to the engine that knows how to convert the target type from some sources
+func (ce *ConverterEngine) AddTargetConverter(targetType reflect.Type, f ConverterFunc) {
+	cf := ce.targetConverters[targetType]
+	cf = append(cf, f)
+	ce.targetConverters[targetType] = cf
+}
+
+func (ce *ConverterEngine) AddInterfaceConverter(interfaceType reflect.Type, f ConverterFunc) {
+	if interfaceType.Kind() != reflect.Interface {
+		panic("type must be an interface")
+	}
+	cf := ce.interfaceConverters[interfaceType]
+	cf = append(cf, f)
+	ce.interfaceConverters[interfaceType] = cf
+}
+
+// convertMap attempts to convert the source map to another type of map
+func (ce *ConverterEngine) convertMap(source interface{}, targetType reflect.Type) (interface{}, error) {
+	S := reflect.ValueOf(source)
+	T := reflect.MakeMap(targetType)
+
+	targetElementType := targetType.Elem()
+	keyType := targetType.Key()
+
+	for i := S.MapRange(); i.Next(); {
+		value, err := ce.Convert(i.Value().Interface(), targetElementType)
+		if err != nil {
+			return nil, err
+		}
+		key, err := ce.Convert(i.Key().Interface(), keyType)
+		if err != nil {
+			return nil, err
+		}
+		T.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(value))
+	}
+	return T.Interface(), nil
+}
+
+// convertSlice attempts to convert a slice to another type of slice
+func (ce *ConverterEngine) convertSlice(source interface{}, targetType reflect.Type) (interface{}, error) {
+	S := reflect.ValueOf(source)
+	T := reflect.MakeSlice(targetType, 0, S.Len())
+	targetElementType := targetType.Elem()
+
+	for i := 0; i < S.Len(); i++ {
+		item, err := ce.Convert(S.Index(i).Interface(), targetElementType)
+		if err != nil {
+			return nil, err
+		}
+		T = reflect.Append(T, reflect.ValueOf(item))
+	}
+	return T.Interface(), nil
+}
+
+// kind2Exact converts a type of the same kind
+func kind2Exact(source interface{}, targetType reflect.Type) interface{} {
+	return reflect.ValueOf(source).Convert(targetType).Interface()
+}
+
+// Convert attempts to convert the source value to the given target type
+// if it does not fail, the returned value is guaranteed to be of the target type
+func (ce *ConverterEngine) Convert(source interface{}, targetType reflect.Type) (interface{}, error) {
+	sourceType := reflect.TypeOf(source)
+	if sourceType == targetType {
+		return source, nil // no conversion necessary
+	}
+
+	// check if there are any custom source converters
+	converters := ce.sourceConverters[reflect.TypeOf(source)]
+	for _, converter := range converters {
+		result, err := converter(source, targetType)
+		if err == nil {
+			return ce.Convert(result, targetType)
+		}
+		if err != ErrNoConversionAvailable {
+			return nil, err
+		}
+	}
+
+	// check if the source type implements ConverterTo
+	converter, ok := source.(ConverterTo)
+	if ok {
+		result, err := converter.ConvertTo(targetType)
+		if err == nil {
+			return ce.Convert(result, targetType)
+		}
+		if err != ErrNoConversionAvailable {
+			return nil, err
+		}
+	}
+
+	// check if there are any custom target converters
+	converters = ce.targetConverters[targetType]
+	for _, converter := range converters {
+		result, err := converter(source, targetType)
+		if err == nil {
+			return ce.Convert(result, targetType)
+		}
+		if err != ErrNoConversionAvailable {
+			return nil, err
+		}
+	}
+
+	// check for interface-based converter (experimental)
+	for itype, converters := range ce.interfaceConverters {
+		for _, converter := range converters {
+			if sourceType.Implements(itype) {
+				result, err := converter(source, targetType)
+				if err == nil {
+					return ce.Convert(result, targetType)
+				}
+				if err != ErrNoConversionAvailable {
+					return nil, err
+				}
+			}
+		}
+	}
+
+	S := reflect.ValueOf(source)
+
+	// Conversion to string
+	if targetType.Kind() == reflect.String {
+		stringer, ok := source.(fmt.Stringer) // if target implements Stringer, use it.
+		if ok {
+			return kind2Exact(stringer.String(), targetType), nil
+		}
+		// Convert to string typical value types
+		switch sourceType.Kind() {
+		case reflect.Bool:
+			return kind2Exact(strconv.FormatBool(S.Bool()), targetType), nil
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			return kind2Exact(strconv.FormatInt(S.Int(), 10), targetType), nil
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			return kind2Exact(strconv.FormatUint(S.Uint(), 10), targetType), nil
+		case reflect.Float32, reflect.Float64:
+			return kind2Exact(strconv.FormatFloat(S.Float(), 'g', 6, int(sourceType.Size())*8), targetType), nil
+		}
+
+	}
+
+	if sourceType.Kind() == reflect.String {
+		// Attempt to parse typical value types from the string
+		switch targetType.Kind() {
+		case reflect.Bool:
+			b, err := strconv.ParseBool(S.String())
+			if err != nil {
+				return nil, err
+			}
+			return kind2Exact(b, targetType), nil
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			i, err := strconv.ParseInt(S.String(), 10, int(targetType.Size())*8)
+			if err != nil {
+				return nil, err
+			}
+			return kind2Exact(i, targetType), nil
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			i, err := strconv.ParseUint(S.String(), 10, int(targetType.Size())*8)
+			if err != nil {
+				return nil, err
+			}
+			return kind2Exact(i, targetType), nil
+		case reflect.Float32, reflect.Float64:
+			f, err := strconv.ParseFloat(S.String(), int(targetType.Size())*8)
+			if err != nil {
+				return nil, err
+			}
+			return kind2Exact(f, targetType), nil
+		}
+	}
+
+	// slice conversion
+	if sourceType.Kind() == reflect.Slice && targetType.Kind() == reflect.Slice {
+		return ce.convertSlice(source, targetType)
+	}
+
+	// map conversion
+	if sourceType.Kind() == reflect.Map && targetType.Kind() == reflect.Map {
+		return ce.convertMap(source, targetType)
+	}
+
+	// reflection-based conversion
+	if reflect.TypeOf(source).ConvertibleTo(targetType) {
+		return S.Convert(targetType).Interface(), nil
+	}
+
+	// no luck
+	return nil, ErrIncompatibleType
+}
+
+// Set sets the given target pointer to sourcevalue, performing
+// any type conversion necessary
+func (ce *ConverterEngine) Set(target, source interface{}) error {
+	T := reflect.ValueOf(target)
+	if T.Kind() != reflect.Ptr {
+		return ErrExpectedPointer
+	}
+	T = T.Elem()
+
+	converted, err := ce.Convert(source, T.Type())
+	if err != nil {
+		return err
+	}
+	T.Set(reflect.ValueOf(converted))
+	return nil
+}
+
+// Convert attempts to convert the source value to the given target type using the default engine
+// if it does not fail, the returned value is guaranteed to be of the target type
+func Convert(source interface{}, targetType reflect.Type) (interface{}, error) {
+	return Default.Convert(source, targetType)
+}
+
+// Set sets the given target pointer to source value using the default engine
+// performing any type conversion necessary
+func Set(target, source interface{}) error {
+	return Default.Set(target, source)
+}
---
+++ convert_test.go
@@ -0,0 +1,226 @@
+package elastic_test
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"math"
+	"reflect"
+	"testing"
+
+	"github.com/epiclabs-io/elastic"
+
+	"github.com/epiclabs-io/ut"
+)
+
+var ErrAny = errors.New("Any error")
+
+type StringAlias string
+type FloatAlias float64
+type IntAlias int
+
+type TestStruct struct {
+	X int
+	Y int
+}
+
+func (ts *TestStruct) String() string {
+	return fmt.Sprintf("(%d, %d)", ts.X, ts.Y)
+}
+
+// implement the converter interface
+func (ts *TestStruct) ConvertTo(targetType reflect.Type) (interface{}, error) {
+	if targetType.Kind() == reflect.Float64 {
+		return math.Sqrt(float64(ts.X*ts.X) + float64(ts.Y*ts.Y)), nil
+	}
+	return nil, elastic.ErrNoConversionAvailable
+}
+
+type ConversionTest struct {
+	source         interface{}
+	expectedResult interface{}
+	expectedError  error
+}
+
+var testData = []ConversionTest{
+	{true, true, nil},
+	{"hello", "hello", nil}, // check no conversion when same types
+	{int8(1), int8(1), nil},
+	{int16(2), int16(2), nil},
+	{int32(3), int32(3), nil},
+	{int64(4), int64(4), nil},
+	{int8(-1), int8(-1), nil},
+	{int16(-2), int16(-2), nil},
+	{int32(-3), int32(-3), nil},
+	{int64(-4), int64(-4), nil},
+	{uint8(5), uint8(5), nil},
+	{uint16(6), uint16(6), nil},
+	{uint32(7), uint32(7), nil},
+	{uint64(8), uint64(8), nil},
+	{float32(9.2), float32(9.2), nil},
+	{float64(194.2), float64(194.2), nil},
+	{"1", int8(1), nil},
+	{"2", int16(2), nil},
+	{"3", int32(3), nil},
+	{"4", int64(4), nil},
+	{"-1", int8(-1), nil},
+	{"-2", int16(-2), nil},
+	{"-3", int32(-3), nil},
+	{"-4", int64(-4), nil},
+	{"5", uint8(5), nil},
+	{"6", uint16(6), nil},
+	{"7", uint32(7), nil},
+	{"8", uint64(8), nil},
+	{"9.2", float32(9.2), nil},
+	{"19.3", float64(19.3), nil},
+	{"-9.2", float32(-9.2), nil},
+	{"-19.3", float64(-19.3), nil},
+	{int8(1), "1", nil},
+	{int16(2), "2", nil},
+	{int32(3), "3", nil},
+	{int64(4), "4", nil},
+	{int8(-1), "-1", nil},
+	{int16(-2), "-2", nil},
+	{int32(-3), "-3", nil},
+	{int64(-4), "-4", nil},
+	{uint8(5), "5", nil},
+	{uint16(6), "6", nil},
+	{uint32(7), "7", nil},
+	{uint64(8), "8", nil},
+	{float32(9.2), "9.2", nil},
+	{float64(19.3), "19.3", nil},
+	{float32(-9.2), "-9.2", nil},
+	{float64(-19.3), "-19.3", nil},
+	{int(-1), uint(0xffffffffffffffff), nil},
+	{"true", true, nil},
+	{"false", false, nil},
+	{true, "true", nil},
+	{false, "false", nil},
+	{5, float32(5), nil},
+	{[]interface{}{1, 2, 3, 4}, []int{1, 2, 3, 4}, nil},
+	{[]interface{}{"1", "2", "3", "-4"}, []int{1, 2, 3, -4}, nil},
+	{[]interface{}{"1.1", "2.2", "3.3", "-4.4"}, []float32{1.1, 2.2, 3.3, -4.4}, nil},
+	{[]interface{}{"1.1", "2.2", "3.3", "-4.4"}, []float64{1.1, 2.2, 3.3, -4.4}, nil},
+	{map[string]interface{}{
+		"uno":  1,
+		"dos":  2,
+		"tres": 3,
+	}, map[string]int{
+		"uno":  1,
+		"dos":  2,
+		"tres": 3,
+	}, nil},
+	{map[string]interface{}{
+		"1": "uno",
+		"2": "dos",
+		"3": "tres",
+	}, map[int]string{
+		1: "uno",
+		2: "dos",
+		3: "tres",
+	}, nil},
+	{[]byte{65, 66, 67, 0}, "ABC\x00", nil},
+	{"ABC\x00", []byte{65, 66, 67, 0}, nil},
+	{"hola", StringAlias("hola"), nil}, // test conversion between alias
+	{StringAlias("hola"), "hola", nil}, // test conversion between alias
+	{"XYZ", int8(1), ErrAny},           // test an unparseable string returns an error
+	{"XYZ", int16(2), ErrAny},
+	{"XYZ", int32(3), ErrAny},
+	{"XYZ", int64(4), ErrAny},
+	{"XYZ", int8(-1), ErrAny},
+	{"XYZ", int16(-2), ErrAny},
+	{"XYZ", int32(-3), ErrAny},
+	{"XYZ", int64(-4), ErrAny},
+	{"XYZ", uint8(5), ErrAny},
+	{"XYZ", uint16(6), ErrAny},
+	{"XYZ", uint32(7), ErrAny},
+	{"XYZ", uint64(8), ErrAny},
+	{"XYZ", float32(9.2), ErrAny},
+	{"XYZ", float64(19.3), ErrAny},
+	{"XYZ", float32(-9.2), ErrAny},
+	{"XYZ", float64(-19.3), ErrAny},
+	{true, 7, ErrAny},
+	{ConversionTest{}, 4, elastic.ErrIncompatibleType},
+	{&TestStruct{X: 5, Y: 7}, "(5, 7)", nil},                      // test fmt.Stringer
+	{&TestStruct{X: 5, Y: 7}, float64(8.602325267042627), nil},    // Test Converter implementation
+	{&TestStruct{X: 5, Y: 7}, FloatAlias(8.602325267042627), nil}, // Test Converter implementation
+	{&TestStruct{X: 5, Y: 7}, StringAlias("(5, 7)"), nil},         // test fmt.Stringer implementation to an alias type
+	{[]byte{0, 0, 0, 1, 0, 0, 0, 2}, TestStruct{X: 1, Y: 2}, nil}, // Test Target converter
+	{&TestStruct{X: 5, Y: 7}, 99, elastic.ErrIncompatibleType},
+	{FloatAlias(2.2), int(2), nil},      // test Source converter
+	{FloatAlias(2.7), int(3), nil},      // test Source converter
+	{FloatAlias(2.7), IntAlias(3), nil}, // test Source converter
+	{float32(5.5), float64(5.5), nil},   // test upgrade/downgrade
+	{float64(5.5), float32(5.5), nil},   // test upgrade/downgrade
+}
+
+func TestConvert(tx *testing.T) {
+	t := ut.BeginTest(tx, false) // set to true to generate test results
+	defer t.FinishTest()
+
+	// The following adds a custom converter that rounds floats when converting them to integers
+	elastic.Default.AddSourceConverter(reflect.TypeOf(FloatAlias(0)), func(source interface{}, targetType reflect.Type) (interface{}, error) {
+		if targetType.Kind() == reflect.Int {
+			f := source.(FloatAlias)
+			d := f - FloatAlias(int(f))
+			if d > 0.5 {
+				return int(f) + 1, nil
+			}
+			return int(f), nil
+		}
+		return nil, elastic.ErrNoConversionAvailable
+	})
+
+	// The following adds a custom target converter that unpacks a TestStruct out of a byte array
+	elastic.Default.AddTargetConverter(reflect.TypeOf(TestStruct{}), func(source interface{}, targetType reflect.Type) (interface{}, error) {
+		switch source.(type) {
+		case []byte:
+			b := source.([]byte)
+			if len(b) == 8 {
+				return TestStruct{
+					X: int(binary.BigEndian.Uint32(b)),
+					Y: int(binary.BigEndian.Uint32(b[4:])),
+				}, nil
+			}
+		}
+		return nil, elastic.ErrNoConversionAvailable
+	})
+
+	// run all tests
+	// each conversion test has a source value that will be converted to the type
+	// of the expected result. Then the result is compared to the expected result.
+	for _, ct := range testData {
+		t.StartSubTest("Conversion of '%v' (%s) to %s", ct.source, reflect.TypeOf(ct.source), reflect.TypeOf(ct.expectedResult).String())
+		r, err := elastic.Convert(ct.source, reflect.TypeOf(ct.expectedResult))
+		if ct.expectedError == nil {
+			t.Ok(err)                      // verify no error
+			t.Equals(ct.expectedResult, r) // compare values to see if conversion was correct
+		} else {
+			if ct.expectedError == ErrAny {
+				t.MustFail(err, "Conversion should have failed")
+			} else {
+				t.MustFailWith(err, ct.expectedError)
+			}
+		}
+
+		// run the same test but using `elastic.Set` instead
+		target := reflect.New(reflect.TypeOf(ct.expectedResult))
+		err = elastic.Set(target.Interface(), ct.source)
+		if ct.expectedError == nil {
+			t.Ok(err)
+			t.Equals(ct.expectedResult, target.Elem().Interface())
+		} else {
+			if ct.expectedError == ErrAny {
+				t.MustFail(err, "Conversion should have failed")
+			} else {
+				t.MustFailWith(err, ct.expectedError)
+			}
+		}
+	}
+
+	// Test `Set` fails when the first parameter is not a pointer
+	var x int
+	err := elastic.Set(x, 4)
+	t.MustFailWith(err, elastic.ErrExpectedPointer)
+
+}
